363ac55dea6cd603a507bea92d163aefeeef2861
elizabethomeara <elizabeth.omeara@mail.mcgill.ca>
2024-09-10 13:13:40 -0400

    beginning assignment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # COMP352A1
 COMP 352 Assignment 1
+
+Welcome to my first ever COMP 352 assignment!  
+To get started, simply run the program from terminal, or use the IntelliJ run function  
+If running from terminal, you can input the assignment question directly into the command line  
+If not, the program will prompt you to input the question  
+Good luck, and thank you :)
+
--- src/com/example/comp352a1.java
+++ src/com/example/comp352a1.java
@@ -3,5 +3,16 @@ package com.example;
 public class comp352a1 {
     public static void main(String[] args) {
 
+        // string arg will refer to the question, q1a q1b etc.
+        if(args.length == 0){
+            //prompt the user to input the question number
+        } else if(args[0].equals("q1")){
+            //run question 1
+
+        }else if(args[0].equals("q2")){
+            //run question 2
+
+        }
+
     }
 }
